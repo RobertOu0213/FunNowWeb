078e48393712e4cda1c0d7933c8f4299eeb11174
liskyo <liskyo5470@gmail.com>
2024-05-31 10:57:23 +0800

    add index2

--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=test}/{id?}");
+    pattern: "{controller=Home}/{action=index}/{id?}");
 
 app.Run();
