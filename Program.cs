03640fff46d0126191d6d4b6fac3a0d36602e8b8
RobertOu0213 <starshining12345@gmail.com>
2024-05-31 23:34:36 +0800

    完成房東首頁

--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=HotelCreate}/{action=Hotel}/{id?}");
+    pattern: "{controller=HostManage}/{action=Home}/{id?}");
 
 app.Run();
