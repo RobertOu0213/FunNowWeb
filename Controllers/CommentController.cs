eae64f95d0caa1688eaa09e9d061fc55291f49bb
Joanna <5525asd9896@gmail.com>
2024-06-02 19:41:57 +0800

    update

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -13,5 +13,10 @@ namespace PrjFunNowWeb.Controllers
         {
             return View();
         }
+
+        public IActionResult comment_page()
+        {
+            return View();
+        }
     }
 }
