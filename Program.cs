3ebc7f0fbfa506e13bbf6620acd42ee120e59d45
louie <louie@gmail.com>
2024-06-04 13:27:01 +0800

    add _layout(platform)

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-pattern: "{controller=PgHotel}/{action=pgHotel}/{id?}");
-//pattern: "{controller=PgBack_Member}/{action=pgBack_Member}/{id?}");
+//pattern: "{controller=PgHotel}/{action=pgHotel}/{id?}");
+pattern: "{controller=PgBack_Member}/{action=pgBack_Member}/{id?}");
 
 app.Run();
