e4fd20cbf3535b9115b8dbc933e42b4d2e18d2c3
juliachiu <juliachiu@gmail.com>
2024-06-04 09:37:06 +0800

    add OwnerHotelsOdersList

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrjFunNowWeb.Models;
+using PrjFunNowWeb.ViewModels;
 using System.Diagnostics;
 
 namespace PrjFunNowWeb.Controllers
@@ -29,6 +30,25 @@ namespace PrjFunNowWeb.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult OwnerHotelsOdersList()
+        {
+            //var orders = from order in _context.Orders
+            //             join detail in _context.OrderDetails on order.OrderID equals detail.OrderID
+            //             join room in _context.Rooms on detail.RoomID equals room.RoomID
+            //             join hotel in _context.Hotels on room.HotelID equals hotel.HotelID
+            //             where room.MemberID == YOUR_MEMBER_ID // 假設房東的 MemberID 已知
+            //             select new OrderViewModel
+            //             {
+            //                 GuestInfo = order.GuestFirstName + " " + order.GuestLastName,
+            //                 HotelName = hotel.HotelName,
+            //                 CheckInDate = detail.CheckInDate,
+            //                 CheckOutDate = detail.CheckOutDate,
+            //                 Price = order.TotalPrice,
+            //                 Status = (order.OrderStatusID == SOME_STATUS_ID) ? "已確認" : "未確認" // 假設狀態ID代表特定狀態
+            //             };
+
+            return View(/*orders.ToList()*/);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
