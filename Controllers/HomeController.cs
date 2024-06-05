fd948a828eaf3c3e2bedeacf3c2977ae3f05666a
ruby <rubyccww@gmail.com>
2024-06-05 14:10:53 +0800

    session key資料可以在首頁顯示

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrjFunNowWeb.Models;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace PrjFunNowWeb.Controllers
 {
@@ -24,8 +25,15 @@ namespace PrjFunNowWeb.Controllers
         public IActionResult Index()
         {
             if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGIN_MEMBER)) //如果有拿到session key
-            return View();
+            {
+                string json = HttpContext.Session.GetString(CDictionary.SK_LOGIN_MEMBER); // 先拿到JSON格式的Session Key
+                
+                Member x = JsonSerializer.Deserialize<Member>(json); //把JSON字串變回<Member>物件
+                
+                ViewData["SessionKey"] = "歡迎會員編號為"+x.MemberId+"的"+x.FirstName+x.LastName+"登入~~~";  //包在ViewData裡面，給其他頁面使用
 
+                return View();
+            }
             return RedirectToAction("Login"); //如果沒拿到session key就回到登入頁面
             
         }
