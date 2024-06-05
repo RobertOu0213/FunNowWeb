44e1e4f653100792e714c4285cfda38242cbaeb2
“Peter” <ezreal1020@gmail.com>
2024-06-05 09:56:43 +0800

    renamebackside hotel page create

---
+++ Controllers/BackSideHotelSystemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrjFunNowWeb.Controllers
+{
+    public class BackSideHotelSystemController : Controller
+    {
+        public IActionResult Hotel()
+        {
+            return View();
+        }
+    }
+}
