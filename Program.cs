294d8e22600741a59330fac2cd7a26617d755d45
sky <liskyo5470@gmail.com>
2024-06-05 11:03:04 +0800

    完成Hotel顯示

--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=test}/{id?}");
+    pattern: "{controller=Home}/{action=index2}/{id?}");
 
 app.Run();
