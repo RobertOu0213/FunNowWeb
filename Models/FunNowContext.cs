b8147d2229533abf46fb453d81613bc9069a6f9f
liskyo <liskyo5470@gmail.com>
2024-06-05 20:12:25 +0800

    修復部分圖片

--- Models/FunNowContext.cs
+++ Models/FunNowContext.cs
@@ -173,7 +173,9 @@ public partial class FunNowContext : DbContext
             entity.Property(e => e.DialogId).HasColumnName("DialogID");
             entity.Property(e => e.CalltoMemberId).HasColumnName("CalltoMemberID");
             entity.Property(e => e.CreateAt).HasColumnType("datetime");
-            entity.Property(e => e.Detail).IsRequired();
+            entity.Property(e => e.Detail)
+                .IsRequired()
+                .IsUnicode(false);
             entity.Property(e => e.MemberId).HasColumnName("MemberID");
 
             entity.HasOne(d => d.Member).WithMany(p => p.Dialogs)
