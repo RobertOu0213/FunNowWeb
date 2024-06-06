d9379738e5e840ff16bca27c9409699d8edce2a1
Julie <juliachiu777@gmail.com>
2024-06-06 15:40:23 +0800

    增加HotelsFavoritesListToCity的口袋清單超連結

--- Controllers/HostManageController.cs
+++ Controllers/HostManageController.cs
@@ -21,9 +21,9 @@ namespace PrjFunNowWeb.Controllers
 
         public IActionResult HostHotelsOdersList()
         {
-            //int memberId = HttpContext.Session.GetInt32("MemberID") ?? 0; // 使用默认值0作为备选
-            int memberId = 1; // 假设房东的 MemberID 是 2
-            var orders = from order in _context.Orders
+            //int memberId = HttpContext.Session.GetInt32("MemberID") ?? 0;         // TODO....  使用默认值0作为备选 等之後登入好了
+            int memberId = 1; // 先寫死  假设房东的 MemberID 是 2
+            var orders = from order in _context.Orders                 //LINQ語法  from...in....
                          join detail in _context.OrderDetails on order.OrderId equals detail.OrderId
                          join room in _context.Rooms on detail.RoomId equals room.RoomId
                          join hotel in _context.Hotels on room.HotelId equals hotel.HotelId
@@ -34,7 +34,7 @@ namespace PrjFunNowWeb.Controllers
                              MemberId = memberId,
                              OrderNumber = order.OrderId,
                              GuestName = order.GuestFirstName + " " + order.GuestLastName,
-                             Nights = (detail.CheckOutDate - detail.CheckInDate).Days,
+                             Nights = (detail.CheckOutDate - detail.CheckInDate).Days,              
                              HotelName = hotel.HotelName,
                              CheckInDate = detail.CheckInDate,
                              CheckOutDate = detail.CheckOutDate,
