9f00812ce9717632e2f2ac3bce221cbf5202cb4b
Joanna <5525asd9896@gmail.com>
2024-06-07 21:34:36 +0800

    local merge

--- Models/FunNowContext.cs
+++ Models/FunNowContext.cs
@@ -80,7 +80,7 @@ public partial class FunNowContext : DbContext
     public virtual DbSet<TravelerType> TravelerTypes { get; set; }
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 //        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=FunNow5;Integrated Security=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -202,7 +202,7 @@ public partial class FunNowContext : DbContext
                 .HasMaxLength(50);
             entity.Property(e => e.HotelTypeId).HasColumnName("HotelTypeID");
             entity.Property(e => e.IsActive)
-                .HasDefaultValueSql("((0))")
+                .HasDefaultValue(false)
                 .HasColumnName("isActive");
             entity.Property(e => e.MemberId).HasColumnName("MemberID");
 
