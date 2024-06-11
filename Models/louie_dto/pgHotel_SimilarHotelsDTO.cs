50813c7e575e6d9aad7231e79c585d9f7b9ebf18
louie <louisthe1998#gmail.com>
2024-06-11 17:13:42 +0800

    圖片綁定: 剩下=>為你推薦輪播區 問題=>飯店彈出視窗, 圖片無法根據超連結下滑=>=>孝宏思考中

--- Models/louie_dto/pgHotel_SimilarHotelsDTO.cs
+++ Models/louie_dto/pgHotel_SimilarHotelsDTO.cs
@@ -8,5 +8,6 @@
         public int LevelStar { get; set; }
         public decimal AverageRoomPrice { get; set; }
         public int AvailableRooms { get; set; }
+        public pgHotel_ImageDTO? HotelImage { get; set; } // 添加这个属性
     }
 }
