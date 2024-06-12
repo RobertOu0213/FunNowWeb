5d5d903d9aa267be8aea2752463b9d69a2e25cdc
“OuCheChun” <starshining12345@gmail.com>
2024-06-12 19:57:14 +0800

    cart AJAX

---
+++ Controllers/cart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrjFunNowWeb.Controllers
+{
+    public class cart : Controller
+    {
+        public IActionResult cartItems()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -88,7 +88,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=HostManage}/{action=HostHotelInfo}/{id?}");
+    pattern: "{controller=cart}/{action=cartItems}/{id?}");
 
 // 配置路由以支持 Angular 路由
 //app.MapFallbackToFile("/dist/fun-now-angular1/index.html");
