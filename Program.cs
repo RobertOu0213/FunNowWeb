027180c922546975ff0ba1be34346992cc1a8796
louie <louisthe1998#gmail.com>
2024-06-13 11:12:26 +0800

    教官正確扣louie的api

--- Program.cs
+++ Program.cs
@@ -82,7 +82,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+//pattern: "{controller=PgHotel}/{action=pgHotel}/{id?}");
 
 // 配置路由以支持 Angular 路由
 app.MapFallbackToFile("/dist/fun-now-angular1/index.html");
