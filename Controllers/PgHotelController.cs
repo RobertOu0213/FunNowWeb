067f847b810e45b9434b0efa956727c888b06a26
louie <louisthe1998#gmail.com>
2024-06-13 16:25:33 +0800

    完成: *只抓room status為true的房間 *抓到session入住日跟退訂日 =>待做: *query string加密 *防null噴錯

--- Controllers/PgHotelController.cs
+++ Controllers/PgHotelController.cs
@@ -14,10 +14,11 @@ namespace PrjFunNowWeb.Controllers
             _httpClient = httpClient;
         }
 
-        public async Task<IActionResult> pgHotel(int hotelId=7)//預設
+        public async Task<IActionResult> pgHotel(int hotelId=7, string checkInDate = null, string checkOutDate = null)//預設
         {
             // 调用API获取数据
-            var response = await _httpClient.GetAsync($"https://localhost:7103/api/pgHotel/{hotelId}");
+            var url = $"https://localhost:7103/api/pgHotel/{hotelId}?checkInDate={checkInDate}&checkOutDate={checkOutDate}";
+            var response = await _httpClient.GetAsync(url);
             if (!response.IsSuccessStatusCode)
             {
                 // 处理错误情况
@@ -44,6 +45,9 @@ namespace PrjFunNowWeb.Controllers
             {
                 hotelDetail.SimilarHotels = new List<pgHotel_SimilarHotelsDTO>();
             }
+
+            hotelDetail.CheckInDate = checkInDate;
+            hotelDetail.CheckOutDate = checkOutDate;
 
             // 将数据传递给视图
             var viewModel = new pgHotel_HotelDetailViewModel
