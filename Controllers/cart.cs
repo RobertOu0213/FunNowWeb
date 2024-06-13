52560999cb0f2d3dd31bcb9768629ac0df5cd969
“OuCheChun” <starshining12345@gmail.com>
2024-06-13 17:26:30 +0800

    cart view 及pg View 畫面

--- Controllers/cart.cs
+++ Controllers/cart.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PrjFunNowWeb.Models;
 
 namespace PrjFunNowWeb.Controllers
 {
     public class cart : Controller
     {
-        public IActionResult cartItems()
+        private readonly FunNowContext _context;
+        public cart(FunNowContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult cartItems(int? userId)
+        {
+            if (userId <= 0)
+            {
+                return BadRequest("UserID is required");
+            }
+
+            var MemberID = _context.Members.Where(x => x.MemberId == userId).Select(x => x.MemberId).FirstOrDefault();
+
+            if (MemberID <= 0)
+            {
+                return NotFound("Member not found");
+            }
+
+            return View(MemberID);
+
         }
     }
 }
