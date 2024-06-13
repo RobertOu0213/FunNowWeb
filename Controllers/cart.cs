a1cf4b0a0717375aedc6baba4e0159feb248a976
RobertOu0213 <starshining12345@gmail.com>
2024-06-14 00:59:36 +0800

    導頁到paymentpage

--- Controllers/cart.cs
+++ Controllers/cart.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrjFunNowWeb.Models;
-
+using PrjFunNowWeb.Models.DTO;
+using System.Text.Json;
 namespace PrjFunNowWeb.Controllers
 {
-    public class cart : Controller
+    public class Cart : Controller
     {
         private readonly FunNowContext _context;
-        public cart(FunNowContext context)
+        public Cart(FunNowContext context)
         {
             _context = context;
         }
+
         public IActionResult cartItems(int? userId)
         {
             if (userId <= 0)
@@ -27,5 +29,38 @@ namespace PrjFunNowWeb.Controllers
             return View(MemberID);
 
         }
+
+        [HttpPost]
+        public IActionResult PreparePayment([FromBody] List<int> orderDetailsId)
+        {
+            if (orderDetailsId == null || !orderDetailsId.Any())
+            {
+                return BadRequest("OrderDetailsId is required");
+            }
+
+   
+            TempData["OrderDetailsId"] = JsonSerializer.Serialize(orderDetailsId);
+
+            var redirectUrl = Url.Action("PaymentPage", "Cart");
+            return Json(new { success = true, redirectUrl });
+        }
+
+
+        public IActionResult PaymentPage()
+        {
+           
+            var orderDetailsIdJson = TempData["OrderDetailsId"] as string;
+
+            if (string.IsNullOrEmpty(orderDetailsIdJson))
+            {
+                return BadRequest("OrderDetailsId is required");
+            }
+
+            var orderDetailsId = JsonSerializer.Deserialize<List<int>>(orderDetailsIdJson);
+            ViewBag.OrderDetailsId = orderDetailsId;
+            return View();
+        }
+
+
     }
 }
