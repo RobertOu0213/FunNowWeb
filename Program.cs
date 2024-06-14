19f135301e0a49e92eac33b6be2ce2dbb049bb96
louie <louisthe1998#gmail.com>
2024-06-14 17:38:11 +0800

    偉誠神救援!! *table圖片不會跑版 *彈出視窗可下滑

--- Program.cs
+++ Program.cs
@@ -91,8 +91,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    //pattern: "{controller=Home}/{action=Index}/{id?}");
-pattern: "{controller=Home}/{action=index}/{id?}");
+pattern: "{controller=Home}/{action=Index}/{id?}");
+//pattern: "{controller=HostManage}/{action=Home}/{id?}");
 
 // 配置路由以支持 Angular 路由
 //app.MapFallbackToFile("/dist/fun-now-angular1/index.html");
