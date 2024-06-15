7a6d4ae76ac2171de1246fac0ad7aa329bea3597
ruby <rubyccww@gmail.com>
2024-06-15 17:31:30 +0800

    登入或登出都可以正常跳轉到第二頁面

--- Program.cs
+++ Program.cs
@@ -91,7 +91,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    
 pattern: "{controller=Home}/{action=index}/{id?}");
 
 // 配置路由以支持 Angular 路由
