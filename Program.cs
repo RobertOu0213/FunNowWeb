d9c6f409d2de1608c1f3fe09965741dd754507f9
“OuCheChun” <starshining12345@gmail.com>
2024-06-18 16:24:06 +0800

    修改json 導致巢狀的問題

--- Program.cs
+++ Program.cs
@@ -69,6 +69,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddSignalR();
 
 
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
