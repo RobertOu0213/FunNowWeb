bae5f2394415339f26be7c56e82371ba8020d1f9
louie <louisthe1998#gmail.com>
2024-06-18 10:37:03 +0800

    *被訂走房間還是會秀, 但押上booked圖案 *地圖圖標訊息, hover就會顯示, 且跟右邊飯店作連動, 會往下滾

--- Models/FunNowContext.cs
+++ Models/FunNowContext.cs
@@ -370,6 +370,10 @@ public partial class FunNowContext : DbContext
             entity.Property(e => e.Birthday).HasColumnType("date");
             entity.Property(e => e.Email).IsRequired();
             entity.Property(e => e.FirstName).IsRequired();
+            entity.Property(e => e.Otpcode).HasColumnName("OTPCode");
+            entity.Property(e => e.Otpexpiry)
+                .HasColumnType("datetime")
+                .HasColumnName("OTPExpiry");
             entity.Property(e => e.Password).IsRequired();
             entity.Property(e => e.Phone)
                 .IsRequired()
