3852b4ff1340f1cf71ba1f9e65b01a96a6876ca4
sky <liskyo5470@gmail.com>
2024-06-19 11:57:42 +0800

    新增房東房客訊息畫面

---
+++ Controllers/HotelManController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrjFunNowWeb.Controllers
+{
+    public class HotelManController : Controller
+    {
+        public IActionResult HotelMessenage()
+        {
+            return View();
+        }
+    }
+}
