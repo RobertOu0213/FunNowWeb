e8312d71ffaf625f3c5c610192a4461d0d2a9e58
Luka <leo10117212000@gmail.com>
2024-06-21 10:11:30 +0800

    客服partial版本搞定+房東行事曆頁面

--- Controllers/HotelManController.cs
+++ Controllers/HotelManController.cs
@@ -54,5 +54,47 @@ namespace PrjFunNowWeb.Controllers
             return View();
 
         }
+        public IActionResult Hoteldaily()
+        {
+            var userID = HttpContext.Session.GetString("MemberID");
+            if (string.IsNullOrEmpty(userID))
+            {
+
+                userID = HttpContext.Session.GetString("GoogleMemberID");
+                if (string.IsNullOrEmpty(userID))
+                {
+                    return RedirectToAction("Login", "Member");
+                }
+            }
+            else
+            {
+                var existingMember = _context.Members
+                    .Where(x => x.MemberId == Convert.ToInt32(userID))
+                    .FirstOrDefault();
+
+                if (existingMember == null)
+                {
+                    return NotFound("Member not found");
+                }
+            }
+
+            var member = _context.Members
+                .Where(x => x.MemberId == Convert.ToInt32(userID))
+                .Select(x => new { x.MemberId, x.FirstName, x.LastName })
+                .FirstOrDefault();
+
+            if (member == null)
+            {
+                return NotFound("Member not found");
+            }
+
+            ViewBag.MemberID = member.MemberId;
+            ViewBag.FirstName = member.FirstName;
+            ViewBag.LastName = member.LastName;
+
+
+            return View();
+
+        }
     }
 }
