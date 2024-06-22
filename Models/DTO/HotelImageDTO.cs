35f66f532624d770a6f9c4d664db7cca4ebb7ef2
“OuCheChun” <starshining12345@gmail.com>
2024-06-22 13:15:58 +0800

    HoteCreate Controller 移至MVC

---
+++ Models/DTO/HotelImageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PrjFunNowWeb.Models.DTO
+{
+    public class HotelImageDTO
+    {
+        public string FileName { get; set; }
+        public string HotelImage { get; set; }
+        public string ImageCategoryID { get; set; }
+    }
+}
---
+++ Models/DTO/RoomDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace PrjFunNowWeb.Models.DTO
+{
+    public class RoomDTO
+    {
+        public string Description { get; set; }
+        public string MaximumOccupancy { get; set; }
+        public List<int> RoomEquipmentID { get; set; }
+        public List<RoomImageDTO> RoomImages { get; set; }
+        public string RoomName { get; set; }
+        public string RoomPrice { get; set; }
+        public string RoomSize { get; set; }
+        public string RoomTypeID { get; set; }
+    }
+}
