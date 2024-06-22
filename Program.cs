ef7b0d41371a7f3c72085ca1609891e884661bce
Joanna <5525asd9896@gmail.com>
2024-06-22 14:01:58 +0800

    目前進度

--- Program.cs
+++ Program.cs
@@ -137,7 +137,7 @@ app.UseCors("AllowAngularApp");
 
 app.MapControllerRoute(
     name: "default",   
-pattern: "{controller=Comment}/{action=Angular_plaform}/{id?}");
+pattern: "{controller=Home}/{action=Index}/{id?}");
 
 //// 配置路由以支持 Angular 路由
 //app.MapFallbackToFile("/dist/fun-now-angular1/index.html");
